789ead0c9e407212c30b079530d762c64a86ad95
Raphleroi7 <raphleroi92i@gmail.com>
2025-10-07 15:20:39 +0200

    Revert "base.php"
    This reverts commit c16e18c0ace764732f17a866b5563fee0acbbea3.

--- base.php
+++ base.php
@@ -3,9 +3,8 @@ function rechercheBySecteur($idSecteur){
     $connect = mysqli_connect("localhost", "root", "", "orbibase");
 
     if ($connect->connect_error) {
-        die("erreur de connexion: " . $connect->connect_error);
+        die("Connection failed: " . $connect->connect_error);
     }
 }
-$sql = "SELECT * FROM `idSecteur` WHERE `id_secteur` = $idSecteur";
 
 ?>
