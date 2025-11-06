ea91a2c273fbcc1c7e752fd9aad05d5dd2d323d6
manue0203 <manueng2006@gmail.com>
2025-11-06 10:49:02 +0100

    Update base.php

---
+++ base.php
@@ -0,0 +1,31 @@
+<?php
+class Base {
+    private PDO $db;
+
+    public function __construct() {
+        try {
+            $this->db = new PDO("mysql:host=localhost;dbname=test", "root", "");
+            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Erreur de connexion : " . $e->getMessage());
+        }
+    }
+
+   
+    public function rechercheBySpecialite(string $nomSpecialite): array {
+        $result = $this->db->prepare("SELECT * FROM specialite WHERE nom LIKE :nom");
+        $result->execute(['nom' => '%' . $nomSpecialite . '%']);
+        return $result->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
+    public function getListSecteur(): array {
+        $result = $this->db->query("SELECT * FROM secteur");
+        return $result->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+   
+    public function getListeSpecialites(): array {
+        $result = $this->db->query("SELECT * FROM specialite");
+        return $result->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
