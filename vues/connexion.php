6e44119da7180ffd08f6d31948d0865fa41f8ef2
BTSSIOAutun <118539814+BTSSIOAutun@users.noreply.github.com>
2025-11-13 11:50:01 +0100

    Modif CSS

--- vues/connexion.php
+++ vues/connexion.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
-    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+    <h1>OrbiBase</h1>
+    <p>Bienvenue ! Veuillez vous connecter pour continuer.</p>
     <form action="" method="post">
         <label for="username">Nom d'utilisateur :</label>
         <input type="text" name="username" id="username" required />
