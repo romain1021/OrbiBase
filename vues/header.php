323062eee0d8d32c724096b24ab3cc4d3ae09604
romain1021 <150360051+romain1021@users.noreply.github.com>
2025-11-17 10:21:23 +0100

    .

--- vues/header.php
+++ vues/header.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/style.css">
-
-    <title>Orbibase</title>
-</head>
-<body>
-    
-</body>
-</html>
-
 <?php
 if (isset($_SESSION['user_id'])) {
     // barre de navigation entre les différentes pages
